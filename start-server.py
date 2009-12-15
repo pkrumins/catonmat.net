63a24db30b971bcba8a3277670b5faf8f08e9473
Peteris Krumins <peteris.krumins@gmail.com>
2009-12-15 21:50:29 +0200

    added a comment what the 5000 means in start_server

--- start-server.py
+++ start-server.py
@@ -4,6 +4,7 @@
 from werkzeug import run_simple
 from catonmat.application import application
 
+# run catonmat application on port 5000
 run_simple('0.0.0.0', 5000, application,
     use_debugger=True, use_reloader=True)
 
