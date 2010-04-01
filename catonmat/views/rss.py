e31ec7a1c3d95de2318911b39aeb900b42c81b3e
Peteris Krumins <peteris.krumins@gmail.com>
2010-04-01 22:54:49 +0300

    catonmat now has atom feed.

---
+++ catonmat/views/rss.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+#
+# Peteris Krumins (peter@catonmat.net)
+# http://www.catonmat.net  --  good coders code, great reuse
+#
+# The new catonmat.net website.
+#
+# Code is licensed under GNU GPL license.
+#
+
+from werkzeug                       import Response, redirect
+from werkzeug.contrib.atom          import AtomFeed
+
+from catonmat.config                import config
+from catonmat.cache                 import cache
+from catonmat.database              import session
+from catonmat.models                import Page, BlogPage
+
+import re
+
+# ----------------------------------------------------------------------------
+
+feedburner_re = re.compile(r'feedburner|feedvalidator', re.I)
+
+def feedburner_bot(request):
+    ua = request.headers.get('User-Agent')
+    if ua and feedburner_re.search(ua):
+        return True
+    return False
+
+def atom_feed(request):
+    if feedburner_bot(request):
+        feed = compute_atom_feed(request)
+        return Response(feed, mimetype='application/atom+xml')
+    return redirect('http://feeds.feedburner.com/catonmat', code=307)
+
+peteris = {
+    'name':  'Peteris Krumins',
+    'uri':   'http://www.catonmat.net/about',
+    'email': 'peter@catonmat.net'
+}
+
+@cache('atom_feed')
+def compute_atom_feed(request):
+    feed = AtomFeed(
+             title     = 'good coders code, great reuse',
+             feed_url  = 'http://www.catonmat.net/feed',
+             url       = 'http://www.catonmat.net',
+             author    = peteris,
+             icon      = 'http://www.catonmat.net/favicon.ico',
+             generator = ('catonmat blog', 'http://www.catonmat.net', 'v1.0')
+           )
+
+             # TODO: logo='http://www.catonmat.net/)
+
+    pages = session. \
+              query(Page). \
+              join(BlogPage). \
+              order_by(BlogPage.publish_date.desc()). \
+              limit(config.rss_items). \
+              all()
+
+    for page in pages:
+        feed.add(title        = page.title,
+                 content      = page.parsed_content,
+                 content_type = 'html',
+                 author       = peteris,
+                 url          = page.request_path,
+                 id           = page.page_id,
+                 updated      = page.last_update,
+                 published    = page.rss_page.publish_date)
+
+    return feed.to_string()
+
