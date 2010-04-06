8da711f15197a36c7f5978cfbc2de5b4abfeda2a
Peteris Krumins <peteris.krumins@gmail.com>
2010-04-06 23:28:45 +0300

    exceptions now get logged

--- catonmat/errorlog.py
+++ catonmat/errorlog.py
@@ -3,21 +3,28 @@
 # Peteris Krumins (peter@catonmat.net)
 # http://www.catonmat.net  --  good coders code, great reuse
 #
-# The new catonmat.net website. See this post for more info:
-# http://www.catonmat.net/blog/50-ideas-for-the-new-catonmat-website/
+# The new catonmat.net website.
 #
 # Code is licensed under GNU GPL license.
 #
 
-from catonmat.models   import FouroFour, Exception
+from catonmat.models    import FouroFour, Exception
+from StringIO           import StringIO
+
+import traceback
+import sys
+
+# ----------------------------------------------------------------------------
 
 def log_404(request):
-    fof = FouroFour(request)
-    fof.save()
+    FouroFour(request).save()
 
-def log_exception(request, e):
-    last_error = str(e)
-    print "---------------------"
-    print last_error
-    print "---------------------"
+def log_exception(request):
+    exc_type, exc_value, tb = sys.exc_info()
+    buffer = StringIO()
+    traceback.print_exception(exc_type, exc_value, tb, file=buffer)
+    Exception(request,
+              str(exc_type),
+              str(exc_value),
+              buffer.getvalue()).save()
 
