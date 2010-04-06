625246db99b78dd5d5b388289a70805b28383c48
Peteris Krumins <peteris.krumins@gmail.com>
2010-04-07 02:39:34 +0300

    added user friendly 404 page

--- catonmat/views/not_found.py
+++ catonmat/views/not_found.py
@@ -1,5 +1,19 @@
-from werkzeug import Response
+#!/usr/bin/python
+#
+# Peteris Krumins (peter@catonmat.net)
+# http://www.catonmat.net  --  good coders code, great reuse
+#
+# The new catonmat.net website.
+#
+# Code is licensed under GNU GPL license.
+#
+
+from werkzeug                           import Response
+from catonmat.views.utils               import render_template
+
+# ----------------------------------------------------------------------------
 
 def main(request):
-    return Response("404 not found: %s" % request.path, mimetype='text/html')
+    template = render_template('404', path=request.path)
+    return Response(template, mimetype='text/html', status=404)
 
