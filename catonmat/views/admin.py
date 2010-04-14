7a21f4bd4f80ae272beccdaa14a48fd4621c7783
Peteris Krumins <peteris.krumins@gmail.com>
2010-04-14 05:15:14 +0300

    can now create new pages and publish them

--- catonmat/views/admin.py
+++ catonmat/views/admin.py
@@ -10,10 +10,12 @@
 
 from werkzeug                       import redirect
 
-from catonmat.models                import session, Page, Revision, Category
+from catonmat.models                import session, Page, Revision, Category, UrlMap, Rss, BlogPage
 from catonmat.views.utils           import display_template, render_template, template_response, MakoDict
 from catonmat.admin                 import require_admin, REQUIRE_IP, logged_in, admin_cred_match_prim, hash_password, mk_secure_cookie
 from catonmat.views.pages           import default_page_template_data, display_page
+
+from datetime                       import datetime
 
 import hashlib
 
@@ -63,8 +65,24 @@ def edit_page(request, page_id):
         return display_template('admin/edit_page', page=page, cats=cats)
     if 'submit' in request.form:
         return edit_page_submit(request, page_id)
+    elif 'publish' in request.form:
+        return publish_page(request, page_id)
     elif 'preview' in request.form:
         return edit_page_preview(request, page_id)
+
+@require_admin()
+def publish_page(request, page_id):
+    page = session.query(Page).filter_by(page_id=page_id).first()
+    if request.form['status'] == 'page':
+        page.status = 'page'
+        page.save()
+    elif request.form['status'] == 'post':
+        page.category.count = page.category.count + 1
+        page.status = 'post'
+        Rss(page, page.last_update).save()
+        BlogPage(page, page.last_update).save()
+        page.save()
+    return redirect('/admin/edit_page/%d' % page.page_id)
 
 @require_admin()
 def edit_page_submit(request, page_id):
@@ -74,8 +92,21 @@ def edit_page_submit(request, page_id):
     page.title = request.form['title']
     page.content = request.form['content']
     page.excerpt = request.form['excerpt']
+    page.last_update = datetime.utcnow()
+
     if page.category.category_id != int(request.form['cat_id']):
         update_category(page, request.form['cat_id'])
+
+    if request.form['request_path']:
+        if page.url_map:
+            page.url_map.request_path = request.form['request_path']
+        else:
+            urlmap = UrlMap(request.form['request_path'], page.page_id).save()
+            page.urlmap = urlmap
+    else:
+        if page.url_map:
+            session.delete(page.url_map)
+
     page.save()
 
     change_note = request.form['change_note'].strip()
@@ -83,8 +114,12 @@ def edit_page_submit(request, page_id):
         Revision(page, change_note).save()
     return display_template('admin/edit_page', page=page, cats=cats)
 
-@require_admin()
 def update_category(page, new_cat_id):
+    if page.status == 'draft':
+        page.category_id = new_cat_id
+        return
+
+    # update page category count only if page is published
     old_cat = page.category
     new_cat = session.query(Category).filter_by(category_id=new_cat_id).first()
 
@@ -93,6 +128,7 @@ def update_category(page, new_cat_id):
 
     page.category = new_cat
 
+@require_admin()
 def edit_page_preview(request, page_id):
     session.autoflush = False
     page = session.query(Page).autoflush(False).filter_by(page_id=page_id).first()
@@ -120,7 +156,15 @@ def new_page_from_request(request):
              request.form['content'],
              request.form['excerpt'],
              category_id=request.form['cat_id'])
+    page.status = 'draft'
     page.save()
+
+    if request.form['request_path']:
+        urlmap = UrlMap(request.form['request_path'], page.page_id).save()
+        page.urlmap = urlmap
+        page.save()
+
+    return page
 
 @require_admin()
 def cats(request):
