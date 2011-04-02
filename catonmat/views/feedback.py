f99540558ede75e90d2762e60c7e94a37a8b93a7
Peteris Krumins <peteris.krumins@gmail.com>
2011-04-01 20:05:18 -0700

    new captcha

--- catonmat/views/feedback.py
+++ catonmat/views/feedback.py
@@ -76,8 +76,8 @@ def validate_feedback(request):
             raise FeedbackError, "You didn't type the message!"
 
     def validate_captcha(captcha):
-        if captcha != "meow":
-            raise FeedbackError, """Please type "meow" in the box below."""
+        if captcha != "green":
+            raise FeedbackError, """Please type "green" in the box below."""
 
     validate_name(request.form['name'].strip())
     validate_email(request.form['email'].strip())
