6857063d3a909fe702ce2f66ee992241b1387c7b
Peteris Krumins <peteris.krumins@gmail.com>
2011-05-21 19:41:03 +0300

    make start-server port configurable

--- start-server.py
+++ start-server.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 #
 
+import sys
 from werkzeug import run_simple
 from catonmat.application import application
 
-# run catonmat application on port 5000
-run_simple('0.0.0.0', 5000, application,
+port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
+run_simple('0.0.0.0', port, application,
     use_debugger=True, use_reloader=True)
 
